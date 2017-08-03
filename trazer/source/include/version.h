ecca695a299f34443653b1f36e4cfa9bcdf71bcc
Darío Baliña <4138736+dariosb@users.noreply.github.com>
2017-08-03 12:57:22 +0000

    Release V3.0

--- trazer/source/include/version.h
+++ trazer/source/include/version.h
@@ -3,9 +3,9 @@
  */
 
 #define VERSION_STRING_TXT	"\n"													\
-							"--- TRAZER V2.5 --- RKH trace event interpreter ---\n"	\
+							"--- TRAZER V3.0 --- RKH trace event interpreter ---\n"	\
 							"\n"													\
 							"          build date:\t"__DATE__"\n" 					\
-							"          tested with:\tRKH V2.4.05\n"
+							"          tested with:\tRKH V3.0.00\n"
 
 
